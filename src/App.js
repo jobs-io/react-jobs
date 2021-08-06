f5a2e508fa20b078b4eca7159696fa900e4c1de7
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2021-08-07 11:40:18 +1200

    adding application bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Menu from './Menu';
 
 function App() {
   return (
     <div className="App">
+      <Menu></Menu>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,9 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-class Menu extends Component {
-  render() {
-    return <div>Menu</div>;
-  }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function Menu() {
+    const classes = useStyles();
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
 }
-
-export default Menu;
